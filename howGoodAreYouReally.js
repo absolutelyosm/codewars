d0de96310cfd31bfe400979d45d1aa506423ef7f
Erin Oh <erinoh@outlook.com>
2023-03-13 21:59:14 -0400

    codewars

---
+++ howGoodAreYouReally.js
@@ -0,0 +1,25 @@
+// <8kyu> There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+// Note:
+
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    let score = 0
+    for (let i = 0; i < classPoints.length; i++) {
+      score += classPoints[i];
+    }
+    return yourPoints >= score / classPoints.length ? true : false
+  }
+
+// OR
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    return yourPoints > classPoints.reduce((a,b) => a + b,0) / classPoints.length;
+  }
